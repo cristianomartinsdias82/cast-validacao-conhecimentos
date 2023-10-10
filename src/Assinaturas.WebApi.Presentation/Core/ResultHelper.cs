f2437865eec690e51cccd895e0665561c56cca02
cristianomartinsdias82 <crisdi@gmail.com>
2023-10-10 20:59:27 -0300

    Added remaining ResultHelper class

---
+++ src/Assinaturas.WebApi.Presentation/Core/ResultHelper.cs
@@ -0,0 +1,21 @@
+﻿using Assinaturas.SharedKernel.Results;
+using Microsoft.AspNetCore.Http;
+
+namespace Assinaturas.WebApi.Presentation.Core;
+
+public static class ResultHelper
+{
+    public static IResult Problem(Failure failure)
+    {
+        if (failure.IsInputValidationError)
+            return Results.ValidationProblem(failure.Failures.ToDictionary(k => k.FieldOrProperty, v => new string[] { v.ErrorCode }));
+
+        if (failure.IsBusinessRuleValidationError)
+            return Results.ValidationProblem(new Dictionary<string, string[]> { ["errorCode"] = new string[] { failure.MessageCode.ToString()! } });
+
+        return Problem(failure.MessageCode);
+    }
+
+    public static IResult Problem(int? errorCode)
+        => Results.Problem(extensions: new Dictionary<string, object?> { [nameof(errorCode)] = errorCode});
+}
